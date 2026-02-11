e4b77fa7f64ae58217b09f44fc76be9062856435
homsi6313-art <homsi6313@gmail.com>
2026-02-11 12:53:10 +0100

    Add linear and binary search implementations
    Implemented linear and binary search functions with performance tests for different array sizes.

--- task-4/search-experiment.js
+++ task-4/search-experiment.js
@@ -1,3 +1,76 @@
-// Write your code here
-// Use generateBigArray to create a big array numbers.
-// Example: generateBigArray(1000000) will create an array of 1 million numbers.
+import generateBigArray from "./bigArray.js";
+
+// Linear Search
+// -------------------------
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+// Binary Search
+// -------------------------
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (array[mid] === target) {
+      return mid;
+    }
+
+    if (array[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+// Generate Arrays (NOT timed)
+// -------------------------
+const array1K = generateBigArray(1000);
+const array100K = generateBigArray(100000);
+const array10M = generateBigArray(10000000);
+
+const target = 123456789; // A number that is not in the arrays to test worst-case scenario
+
+// Linear Search Tests
+// -------------------------
+console.time("Linear Search 1K");
+linearSearch(array1K, target);
+console.timeEnd("Linear Search 1K");
+
+console.time("Linear Search 100K");
+linearSearch(array100K, target);
+console.timeEnd("Linear Search 100K");
+
+console.time("Linear Search 10M");
+linearSearch(array10M, target);
+console.timeEnd("Linear Search 10M");
+
+console.log("----------------------------");
+
+// Binary Search Tests
+// -------------------------
+console.time("Binary Search 1K");
+binarySearch(array1K, target);
+console.timeEnd("Binary Search 1K");
+
+console.time("Binary Search 100K");
+binarySearch(array100K, target);
+console.timeEnd("Binary Search 100K");
+
+console.time("Binary Search 10M");
+binarySearch(array10M, target);
+console.timeEnd("Binary Search 10M");
+// Reflection:
+// Binary Search was much faster than Linear Search,
+// especially for large arrays because it has O(log n)
+// complexity compared to O(n).
